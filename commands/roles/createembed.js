4f8061f6afbf22a7b59bbd21a68d69fca8e04865
TheRealCrazyfuy <TheRealCrazyfuy@users.noreply.github.com>
2025-07-04 15:52:04 +0200

    Removed channel id config and added the option to select the channel in the /createembed command

--- commands/roles/createembed.js
+++ commands/roles/createembed.js
@@ -5,26 +5,32 @@ const roles = require('../../data/roles.json');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('createembed')
-        .setDescription('Create the reaction roles embed'),
+        .setDescription('Create the reaction roles embed')
+        .addChannelOption(option =>
+            option
+                .setName('channel')
+                .setDescription('The channel to create the roles dropdowns in')
+                .setRequired(true)
+        ),
     async execute(interaction) {
 
-		if (!interaction.member.permissions.has('ManageRoles')) {
-			return interaction.reply({ content: `Good try, but you can't use this bot <a:myredmagicreaction:1313432136367472681> `, ephemeral: true });
-		}
+        if (!interaction.member.permissions.has('ManageRoles')) {
+            return interaction.reply({ content: `Good try, but you can't use this bot <a:myredmagicreaction:1313432136367472681> `, ephemeral: true });
+        }
 
         const targetChannelId = config.reactRolesChannelId;
 
-        const channel = await interaction.guild.channels.fetch(targetChannelId);
+        const channel = interaction.options.getChannel('channel') || interaction.guild.channels.cache.get(targetChannelId);
         if (!channel || !channel.isTextBased()) {
-            return interaction.reply({ content: 'Can not find the reaction roles channel smh.', ephemeral: true });
+            return interaction.reply({ content: 'Can not find the channel smh.', ephemeral: true });
         }
 
         const phoneDropdown = new ActionRowBuilder().addComponents(
             new StringSelectMenuBuilder()
                 .setCustomId('phoneDropdown')
                 .setPlaceholder('Choose a phone')
-				.setMinValues(1)
-        		.setMaxValues(3)
+                .setMinValues(1)
+                .setMaxValues(3)
                 .addOptions(roles.phones.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })))
         );
 
@@ -32,8 +38,8 @@ module.exports = {
             new StringSelectMenuBuilder()
                 .setCustomId('accessoryDropdown')
                 .setPlaceholder('Choose an accesory')
-				.setMinValues(1)
-        		.setMaxValues(1)
+                .setMinValues(1)
+                .setMaxValues(1)
                 .addOptions(roles.accessories.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })))
         );
 
@@ -41,20 +47,21 @@ module.exports = {
             new StringSelectMenuBuilder()
                 .setCustomId('pcDropdown')
                 .setPlaceholder('Choose a PC peripheral')
-				.setMinValues(1)
-        		.setMaxValues(2)
+                .setMinValues(1)
+                .setMaxValues(2)
                 .addOptions(roles.pcPeripherals.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })))
         );
 
-		await channel.send({ content: `Please take your cosmetic Roles here.
+        await channel.send({
+            content: `Please take your cosmetic Roles here.
 In case you own more devices, feel free to ask a Mod.
 
 To remove all your roles choose the "Remove all" option in the dropdown.
 ` });
-		await channel.send({ content: '-# You can choose up to 3 Phone roles', components: [phoneDropdown] });
+        await channel.send({ content: '-# You can choose up to 3 Phone roles', components: [phoneDropdown] });
         await channel.send({ content: '-# You can choose up to 1 Accessory roles', components: [accessoryDropdown] });
         await channel.send({ content: '-# You can choose up to 2 PC peripheral roles', components: [pcDropdown] });
 
-		await interaction.reply({ content: 'Done!', ephemeral: true });
+        await interaction.reply({ content: 'Done!', ephemeral: true });
     },
 };
