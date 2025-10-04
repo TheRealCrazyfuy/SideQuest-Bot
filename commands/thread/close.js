36c8cd486b7db4b1e5b31aeed457ff6100b99ad7
TheRealCrazyfuy <TheRealCrazyfuy@users.noreply.github.com>
2025-10-04 21:36:11 +0200

    add close thread confirmation

--- commands/thread/close.js
+++ commands/thread/close.js
@@ -1,5 +1,4 @@
-const { SlashCommandBuilder, MessageFlags } = require('discord.js');
-const { solvedTagId } = require('../../config.json');
+const { SlashCommandBuilder, MessageFlags, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,24 +9,13 @@ module.exports = {
         if (interaction.channel.isThread()) {
             try {
                 if (interaction.user.id == interaction.channel.ownerId || interaction.member.permissions.has('ManageThreads')) {
-                    // find the solved tag in the parent channel from tag id
-                    const solvedTag = interaction.channel.parent.availableTags.find(
-                        t => t.id == solvedTagId
+                    const row = new ActionRowBuilder().addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('confirm_close_thread')
+                            .setLabel('✅Confirm Close')
+                            .setStyle(ButtonStyle.Danger),
                     );
-
-                    // get current tags and add the solved tag if it's not already there
-                    let updatedTags = [...(interaction.channel.appliedTags || [])];
-                    if (updatedTags.length >= 5) {
-                        updatedTags = updatedTags.slice(0, 4); // remove the last tag if there are already 5 tags
-                    }
-                    if (!updatedTags.includes(solvedTag.id)) {
-                        updatedTags.push(solvedTag.id);
-                    }
-                    await interaction.channel.setAppliedTags(updatedTags);
-
-                    await interaction.channel.setLocked(true);
-                    await interaction.reply({ content: 'Thread has been closed successfully <:Mora_Agree:1380160309771374624>.' });
-                    await interaction.channel.setArchived(true);
+                    await interaction.reply({ content: 'Do you want to close this thread?', components: [row], flags: MessageFlags.Ephemeral });
                 } else {
                     await interaction.reply({ content: 'You do not have permission to close this thread <:MoraCabbage:1222196835142205471>.', flags: MessageFlags.Ephemeral });
                 }
