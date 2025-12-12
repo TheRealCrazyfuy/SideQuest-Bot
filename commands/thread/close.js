8c0f5d390264a27766ec2766fb20432001591975
TheRealCrazyfuy <TheRealCrazyfuy@users.noreply.github.com>
2025-12-12 20:28:30 +0100

    Format project

--- commands/thread/close.js
+++ commands/thread/close.js
@@ -1,30 +1,54 @@
-const { SlashCommandBuilder, MessageFlags, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const {
+  SlashCommandBuilder,
+  MessageFlags,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('close')
-        .setDescription('Closes the current thread'),
-    async execute(interaction) {
-        // Close the thread
-        if (interaction.channel.isThread()) {
-            try {
-                if (interaction.user.id == interaction.channel.ownerId || interaction.member.permissions.has('ManageThreads')) {
-                    const row = new ActionRowBuilder().addComponents(
-                        new ButtonBuilder()
-                            .setCustomId('confirm_close_thread')
-                            .setLabel('✅Confirm Close')
-                            .setStyle(ButtonStyle.Danger),
-                    );
-                    await interaction.reply({ content: 'Do you want to close this thread?', components: [row], flags: MessageFlags.Ephemeral });
-                } else {
-                    await interaction.reply({ content: 'You do not have permission to close this thread <:MoraCabbage:1222196835142205471>.', flags: MessageFlags.Ephemeral });
-                }
-            } catch (err) {
-                console.error('Error closing thread:', err);
-                await interaction.reply({ content: 'There was an error closing the thread.', flags: MessageFlags.Ephemeral });
-            }
+  data: new SlashCommandBuilder()
+    .setName("close")
+    .setDescription("Closes the current thread"),
+  async execute(interaction) {
+    // Close the thread
+    if (interaction.channel.isThread()) {
+      try {
+        if (
+          interaction.user.id == interaction.channel.ownerId ||
+          interaction.member.permissions.has("ManageThreads")
+        ) {
+          const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+              .setCustomId("confirm_close_thread")
+              .setLabel("✅Confirm Close")
+              .setStyle(ButtonStyle.Danger),
+          );
+          await interaction.reply({
+            content: "Do you want to close this thread?",
+            components: [row],
+            flags: MessageFlags.Ephemeral,
+          });
         } else {
-            await interaction.reply({ content: 'This command can only be used in a thread <:Mora_Scream:1388103624181158020>.', flags: MessageFlags.Ephemeral });
+          await interaction.reply({
+            content:
+              "You do not have permission to close this thread <:MoraCabbage:1222196835142205471>.",
+            flags: MessageFlags.Ephemeral,
+          });
         }
-    },
-}
+      } catch (err) {
+        console.error("Error closing thread:", err);
+        await interaction.reply({
+          content: "There was an error closing the thread.",
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    } else {
+      await interaction.reply({
+        content:
+          "This command can only be used in a thread <:Mora_Scream:1388103624181158020>.",
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  },
+};
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
 # SideQuest bot
+
 SideQuest is the official bot of the [REDMAGIC Community Discord server](https://discord.gg/redmagic-global-community-897390969744424980). It manages the self-role channel and the help and support forum, helping users choose their roles and offering quick help to their problems.
 
 ## Installation
+
 1. Clone the repository
 2. Rename `example-config.json` to `config.json`
 3. Add your bot data to `config.json`
