d035510b5874d4dc07b4834344f0bf045fc1e546
TheRealCrazyfuy <108433136+TheRealCrazyfuy@users.noreply.github.com>
2026-05-14 19:35:02 +0200

    add new member analysis test

--- commands/test/test.js
+++ commands/test/test.js
@@ -1,4 +1,4 @@
-const { logStandardMessage } = require('../../utils/logging');
+const { logStandardMessage, logHeuristicWarning } = require('../../utils/logging');
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
@@ -14,7 +14,13 @@ module.exports = {
                         .setDescription('The message to log')
                         .setRequired(true)
                 ),
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('analyzenewmember')
+                .setDescription('test analyze new member')
         ),
+
     async execute(interaction) {
         if (!interaction.member.permissions.has('ManageRoles') && !interaction.member.roles.cache.some(role => whitelistedRoleIds.includes(role.id))) {
             return interaction.reply({ content: `Good try, but you can't use this command <a:myredmagicreaction:1313432136367472681> `, flags: MessageFlags.Ephemeral });
@@ -27,6 +33,13 @@ module.exports = {
                 const message = interaction.options.getString('message');
                 await logStandardMessage(message, interaction.client);
                 await interaction.reply({ content: 'Test log sent!', flags: MessageFlags.Ephemeral });
+                break;
+            case 'analyzenewmember':
+                const member = interaction.member;
+                const { calculateHeuristicScore } = require('./../../utils/heuristics');
+                const score = await calculateHeuristicScore(member.user, member.client);
+                logHeuristicWarning(member.user, score, member.client);
+                await interaction.reply({ content: 'Check the logs for the analysis.', flags: MessageFlags.Ephemeral });
                 break;
             default:
                 await interaction.reply({ content: 'Unknown subcommand.', flags: MessageFlags.Ephemeral });
