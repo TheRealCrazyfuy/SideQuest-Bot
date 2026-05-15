acbf82fd0aed546096726eaa36365242a28c3044
TheRealCrazyfuy <108433136+TheRealCrazyfuy@users.noreply.github.com>
2026-05-15 15:34:20 +0200

    use modal to choose roles

--- commands/roles/createdropdowns.js
+++ commands/roles/createdropdowns.js
@@ -1,7 +1,7 @@
-const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, MessageFlags } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, ModalBuilder, TextDisplayBuilder, MessageFlags, ButtonBuilder, LabelBuilder } = require('discord.js');
 const config = require('../../config.json');
 const roles = require('../../data/roles.json');
-const { logStandardMessage } = require('../../utils/logging');
+const { logStandardMessage, logSetRoles } = require('../../utils/logging');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -26,55 +26,127 @@ module.exports = {
             return interaction.reply({ content: 'Can not find the channel smh.', flags: MessageFlags.Ephemeral });
         }
 
-        const phoneDropdown = new ActionRowBuilder().addComponents(
-            new StringSelectMenuBuilder()
-                .setCustomId('phoneDropdown')
-                .setPlaceholder('Choose a phone')
-                .setMinValues(1)
-                .setMaxValues(3)
-                .addOptions(roles.phones.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })))
-        );
+        const button = new ButtonBuilder()
+            .setCustomId('open_cosmetic_roles_modal')
+            .setLabel('Select roles')
+            .setStyle('Primary');
 
-        const tabletDropdown = new ActionRowBuilder().addComponents(
-            new StringSelectMenuBuilder()
-                .setCustomId('tabletDropdown')
-                .setPlaceholder('Choose a tablet')
-                .setMinValues(1)
-                .setMaxValues(2)
-                .addOptions(roles.tablets.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })))
-        );
-
-        const accessoryDropdown = new ActionRowBuilder().addComponents(
-            new StringSelectMenuBuilder()
-                .setCustomId('accessoryDropdown')
-                .setPlaceholder('Choose an accesory')
-                .setMinValues(1)
-                .setMaxValues(1)
-                .addOptions(roles.accessories.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })))
-        );
-
-        const pcDropdown = new ActionRowBuilder().addComponents(
-            new StringSelectMenuBuilder()
-                .setCustomId('pcDropdown')
-                .setPlaceholder('Choose a PC peripheral')
-                .setMinValues(1)
-                .setMaxValues(2)
-                .addOptions(roles.pcPeripherals.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })))
-        );
+        const row = new ActionRowBuilder()
+            .addComponents(button);
 
         await channel.send({
-            content: `Please take your cosmetic Roles here.
-In case you own more devices, feel free to ask a Mod.
-
-To remove all your roles choose the "Remove all" option in the dropdown.
-` });
-        await channel.send({ content: '-# You can choose up to 3 Phone roles', components: [phoneDropdown] });
-        await channel.send({ content: '-# You can choose up to 2 Tablet roles', components: [tabletDropdown] });
-        await channel.send({ content: '-# You can choose up to 1 Accessory roles', components: [accessoryDropdown] });
-        await channel.send({ content: '-# You can choose up to 2 PC peripheral roles', components: [pcDropdown] });
+            content: 'Please take your cosmetic Roles here.\nIn case you own more devices, feel free to ask a Mod.', components: [row]
+        });
 
         await interaction.reply({ content: 'Done!', flags: MessageFlags.Ephemeral });
 
         logStandardMessage(`Roles dropdowns created in <#${channel.id}> by <@${interaction.user.id}>.`, interaction.client);
+    },
+
+    async handleButtonInteraction(interaction) {
+        const modal = new ModalBuilder()
+            .setCustomId('cosmetic_roles_modal')
+            .setTitle('Select your roles');
+
+        const infoText = new TextDisplayBuilder()
+            .setContent('You can select your cosmetic roles below.\n:information_source: If you selected roles before, you will have to select them again or they will be cleared.\n\n:warning: Please only take the roles of devices you actually own <:Dreamy_Mora:1387802057309819010>');
+
+        const phoneDropdown = new StringSelectMenuBuilder()
+            .setRequired(false)
+            .setCustomId('phoneDropdown')
+            .setPlaceholder('Choose a phone')
+            .setMinValues(0)
+            .setMaxValues(3)
+            .addOptions(roles.phones.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })));
+
+        const tabletDropdown = new StringSelectMenuBuilder()
+            .setRequired(false)
+            .setCustomId('tabletDropdown')
+            .setPlaceholder('Choose a tablet')
+            .setMinValues(0)
+            .setMaxValues(2)
+            .addOptions(roles.tablets.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })));
+
+        const accessoryDropdown = new StringSelectMenuBuilder()
+            .setRequired(false)
+            .setCustomId('accessoryDropdown')
+            .setPlaceholder('Choose an accessory')
+            .setMinValues(0)
+            .setMaxValues(1)
+            .addOptions(roles.accessories.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })));
+
+        const pcDropdown = new StringSelectMenuBuilder()
+            .setRequired(false)
+            .setCustomId('pcDropdown')
+            .setPlaceholder('Choose a PC peripheral')
+            .setMinValues(0)
+            .setMaxValues(2)
+            .addOptions(roles.pcPeripherals.map(opt => ({ label: opt.label, value: opt.value, description: opt.description || undefined })));
+
+        modal.addTextDisplayComponents(infoText);
+        modal.addLabelComponents(
+            new LabelBuilder().setLabel('You can choose up to 3 Phone roles').setStringSelectMenuComponent(phoneDropdown),
+            new LabelBuilder().setLabel('You can choose up to 2 Tablet roles').setStringSelectMenuComponent(tabletDropdown),
+            new LabelBuilder().setLabel('You can choose 1 Accessory role').setStringSelectMenuComponent(accessoryDropdown),
+            new LabelBuilder().setLabel('You can choose up to 2 PC Peripheral roles').setStringSelectMenuComponent(pcDropdown),
+        );
+
+        await interaction.showModal(modal);
+
+    },
+
+    async handleModalSubmit(interaction) {
+
+        const rolesData = require('../../data/roles.json');
+
+        const selectedPhoneRoles = interaction.fields.getStringSelectValues('phoneDropdown') || [];
+        const selectedTabletRoles = interaction.fields.getStringSelectValues('tabletDropdown') || [];
+        const selectedAccessoryRoles = interaction.fields.getStringSelectValues('accessoryDropdown') || [];
+        const selectedPcRoles = interaction.fields.getStringSelectValues('pcDropdown') || [];
+
+        const selectedValues = [
+            ...selectedPhoneRoles,
+            ...selectedTabletRoles,
+            ...selectedAccessoryRoles,
+            ...selectedPcRoles,
+        ];
+
+        const allRoles = [
+            ...rolesData.phones,
+            ...rolesData.tablets,
+            ...rolesData.accessories,
+            ...rolesData.pcPeripherals,
+        ];
+
+        const allRoleIds = allRoles.map(role => role.roleId).filter(Boolean);
+        const member = interaction.member;
+
+        for (const roleId of allRoleIds) {
+            if (member.roles.cache.has(roleId)) {
+                try {
+                    await member.roles.remove(roleId);
+                } catch (err) {
+                    console.error(`Failed to remove role ${roleId}:`, err);
+                }
+            }
+        }
+
+        const roleIdsToAdd = allRoles
+            .filter(role => selectedValues.includes(role.value))
+            .map(role => role.roleId)
+            .filter(Boolean);
+
+        for (const roleId of roleIdsToAdd) {
+            try {
+                await member.roles.add(roleId);
+            } catch (err) {
+                console.error(`Failed to add role ${roleId}:`, err);
+            }
+        }
+        logSetRoles(interaction.user, roleIdsToAdd, interaction.client);
+        await interaction.reply({
+            content: `Updated your roles <:mora_popcorn:1133350731357896744>\nNote: Please only take the roles of devices you actually own <:Dreamy_Mora:1387802057309819010>`,
+            flags: MessageFlags.Ephemeral,
+        });
     },
 };
